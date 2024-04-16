3f6a8283b77cd1da7255299718637e542201010a
강태연 <kangty1028@naver.com>
2024-04-16 10:18:18 +0900

    Chore: 백엔드 초기 구성

---
+++ backend/src/main/java/com/ssafy/challs/ChallssApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.challs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallssApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/challs/ChallssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.challs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
