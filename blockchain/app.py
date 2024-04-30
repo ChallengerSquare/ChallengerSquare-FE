85cd2d79d94e393ec324284701a30d1965bfb4f9
Junhyun An <alloy0301@gmail.com>
2024-04-30 13:41:34 +0900

    [S10P31A205-221] Delete : sample request code

--- blockchain/app.py
+++ blockchain/app.py
@@ -49,18 +49,6 @@ def is_valid():
     return jsonify(response), 200
 
 
-# @app.route('/add_sample_transaction', methods=['POST'])
-# def add_sample_transaction():
-#     json = request.get_json()
-#     transaction_keys = ['sender', 'receiver', 'amount']
-#     if not all(key in json for key in transaction_keys):
-#         return 'Some elements of the transaction are missing', 400
-#
-#     index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
-#     response = {'message': f'This transaction will be added to Block {index}'}
-#     return jsonify(response), 201
-
-
 @app.route('/add_transaction', methods=['POST'])
 def add_transaction():
     json = request.get_json()
