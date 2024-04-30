130dbe01284268b7688c0e360cd4344a75abcc36
강다솔 <dasol199@naver.com>
2024-04-30 15:47:54 +0900

    [S10P31A205-208] Test: 알림 조회 테스트 코드 추가

---
+++ backend/src/test/java/com/ssafy/challs/domain/alert/controller/AlertControllerTest.java
@@ -0,0 +1,80 @@
+package com.ssafy.challs.domain.alert.controller;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.ssafy.challs.domain.alert.dto.response.AlertResponseDto;
+import com.ssafy.challs.domain.alert.service.AlertService;
+
+@WebMvcTest(AlertController.class)
+@MockBean(JpaMetamodelMappingContext.class)  // @EnableJpaAuditing
+class AlertControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	private AlertService alertService;
+
+	@Test
+	@DisplayName("알림조회_모든알림_성공")
+	void searhAlertListTest() throws Exception {
+
+		// 테스트 데이터 준비
+		List<AlertResponseDto> allAlerts = createTestData();
+		when(alertService.findAlerts("testMember", false)).thenReturn(allAlerts);
+
+		// 테스트 실행
+		mockMvc.perform(MockMvcRequestBuilders.get("/alert")
+				.param("memberCode", "testMember"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.data", hasSize(2)));
+	}
+
+	@Test
+	@DisplayName("알림조회_안읽은알림_성공")
+	void searhUnreadAlertListTest() throws Exception {
+
+		// 테스트 데이터 준비
+		List<AlertResponseDto> unreadAlerts = createTestData();
+		when(alertService.findAlerts("testMember", true)).thenReturn(unreadAlerts);
+
+		// 테스트 실행
+		mockMvc.perform(MockMvcRequestBuilders.get("/alert")
+				.param("memberCode", "testMember")
+				.param("unread", "true"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.data", org.hamcrest.Matchers.hasSize(2)));
+
+	}
+
+	private List<AlertResponseDto> createTestData() {
+		AlertResponseDto test1 = AlertResponseDto.builder()
+			.alertId(1L)
+			.alertContent("테스트알림1")
+			.alertType('N')
+			.alertTargetId(1L)
+			.isRead(false).build();
+		AlertResponseDto test2 = AlertResponseDto.builder()
+			.alertId(2L)
+			.alertContent("테스트알림2")
+			.alertType('Q')
+			.alertTargetId(1L)
+			.isRead(false).build();
+		return Arrays.asList(test1, test2);
+	}
+
+}
