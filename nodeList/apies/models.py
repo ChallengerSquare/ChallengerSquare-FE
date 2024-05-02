3d1bf9374be0c9df66b93e9abe879e8f4e2bf782
CocoIsCat <demy176@naver.com>
2024-05-02 14:17:24 +0900

    [S10P31A205-219] Feat: 노드 모델 생성

--- nodeList/apies/models.py
+++ nodeList/apies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class NodeList(models.Model):
+    ID = models.AutoField(primary_key=True)
+    ADDR = models.CharField(max_length=30)
+    ADD_DATE = models.DateTimeField(auto_now_add=True)
+    DEL_DATE = models.DateTimeField(null=True, blank=True)
+    LAST_CONN_DATE = models.DateTimeField(null=True, blank=True)
