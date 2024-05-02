5704546811697bb4a041b67f14fcf7545937eb9a
CocoIsCat <demy176@naver.com>
2024-05-02 16:03:51 +0900

    [S10P31A205-219] Feat: 노드 추가 api 매핑

--- nodeList/apies/urls.py
+++ nodeList/apies/urls.py
@@ -1,7 +1,6 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    path('connect/',),  # 노드 연결 요청
-    path('list/',)  # 노드 목록 요청
+    path('connect',views.connectNode),  # 노드 연결 요청
 ]
