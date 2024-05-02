17306ee5e4ec45b65203b683ca89a91a78a01d74
CocoIsCat <demy176@naver.com>
2024-05-02 16:11:41 +0900

    [S10P31A205-219] Fix: return이 없어서 나던 오류 해결

--- nodeList/apies/views.py
+++ nodeList/apies/views.py
@@ -15,6 +15,8 @@ def connectNode(request):
     node = Node()
     data = json.loads(request.body)
     node.ADDR = data['addr']
+    # todo: IP 중복이면 저장 안하는 로직 추가하기
     node.ADD_DATE = datetime.datetime.now()
     node.LAST_CONN_DATE = datetime.datetime.now()
     node.save()
+    return Response(data)
