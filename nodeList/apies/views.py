ca76bee41f7d8bb71501c6c138f3a55e91002dca
CocoIsCat <demy176@naver.com>
2024-05-02 16:05:07 +0900

    [S10P31A205-219] Feat: 노드 추가 함수 구현

--- nodeList/apies/views.py
+++ nodeList/apies/views.py
@@ -1,3 +1,20 @@
+import json
+import datetime
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Node
+from django.http.response import HttpResponse
+import requests
 
 # Create your views here.
+
+@api_view(['POST'])
+def connectNode(request):
+    node = Node()
+    data = json.loads(request.body)
+    node.ADDR = data['addr']
+    node.ADD_DATE = datetime.datetime.now()
+    node.LAST_CONN_DATE = datetime.datetime.now()
+    node.save()
