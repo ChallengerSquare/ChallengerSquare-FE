e8409e69fc42971cc8c439a0ba30ccfecd6961d4
강다솔 <dasol199@naver.com>
2024-05-02 16:54:12 +0900

    [S10P31A205-192] Feat: Contest 정보 DB에 저장할 repository 추가

---
+++ backend/src/main/java/com/ssafy/challs/domain/contest/repository/ContestRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.challs.domain.contest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.challs.domain.contest.entity.Contest;
+
+public interface ContestRepository extends JpaRepository<Contest, Long> {
+}
