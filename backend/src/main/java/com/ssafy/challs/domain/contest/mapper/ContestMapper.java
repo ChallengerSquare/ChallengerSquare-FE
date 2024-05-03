270deef6a630d79b48874955fac7affa41cb7fcf
강다솔 <dasol199@naver.com>
2024-05-03 11:03:55 +0900

    [S10P31A205-192] Feat: 수정 Dto -> Contest 매퍼 추가

--- backend/src/main/java/com/ssafy/challs/domain/contest/mapper/ContestMapper.java
+++ backend/src/main/java/com/ssafy/challs/domain/contest/mapper/ContestMapper.java
@@ -5,6 +5,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import com.ssafy.challs.domain.contest.dto.request.ContestCreateRequestDto;
+import com.ssafy.challs.domain.contest.dto.request.ContestUpdateRequestDto;
 import com.ssafy.challs.domain.contest.dto.response.ContestAwardsDto;
 import com.ssafy.challs.domain.contest.entity.Awards;
 import com.ssafy.challs.domain.contest.entity.Contest;
@@ -24,4 +25,14 @@ public interface ContestMapper {
 
 	@Mapping(source = "contest", target = "contest")
 	Awards awardsDtoToEntity(ContestAwardsDto contestAwardsDto, Contest contest);
+
+	@Mapping(source = "image", target = "contestImage")
+	@Mapping(source = "contestRequestDto.contestId", target = "id")
+	@Mapping(source = "team", target = "team")
+	@Mapping(source = "contestRequestDto.registrationPeriod.start", target = "contestRegistrationStart")
+	@Mapping(source = "contestRequestDto.registrationPeriod.end", target = "contestRegistrationEnd")
+	@Mapping(source = "contestRequestDto.contestPeriod.start", target = "contestStart")
+	@Mapping(source = "contestRequestDto.contestPeriod.end", target = "contestEnd")
+	Contest contestUpdateDtoToContest(ContestUpdateRequestDto contestRequestDto, Team team, String image,
+		Character contestState);
 }
