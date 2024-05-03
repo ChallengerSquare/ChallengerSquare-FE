c6b75b2c5595e9d4de3b37b8eacc6fcd92b23076
CocoIsCat <demy176@naver.com>
2024-05-03 10:52:41 +0900

    [S10P31A205-219] Feat: 노드 리스트 반환 API 구현

--- nodeList/apies/views.py
+++ nodeList/apies/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Node
+from . import serializers
 from django.http.response import HttpResponse
 import requests
 
@@ -19,4 +20,12 @@ def connectNode(request):
     node.ADD_DATE = datetime.datetime.now()
     node.LAST_CONN_DATE = datetime.datetime.now()
     node.save()
+    # todo: 리턴 값 제대로 설정하기
     return Response(data)
+
+@api_view(['GET'])
+def listNodes(request):
+    nodes = Node.objects.all()
+    # print(nodes)
+    serializer = serializers.NodeSerializer(nodes, many=True)
+    return Response(serializer.data)
