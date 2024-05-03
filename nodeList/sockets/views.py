f8531d148acb4398aa31b5b81986bc0123a766b4
CocoIsCat <demy176@naver.com>
2024-05-03 17:55:21 +0900

    [S10P31A205-247] Feat: 집 가기 전 커밋

--- nodeList/sockets/views.py
+++ nodeList/sockets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from socket import *
+from ssl import *
 # Create your views here.
+
+host = "127.0.0.1"
+port = 5000
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
