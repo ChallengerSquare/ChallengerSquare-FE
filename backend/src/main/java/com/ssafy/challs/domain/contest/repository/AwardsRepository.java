8fbaf50c88bfff7ad8ec7f0ac4a435d181f2489a
강다솔 <dasol199@naver.com>
2024-05-03 11:06:53 +0900

    [S10P31A205-192] Feat: 대회로 Awards 조회 / 삭제 추가

--- backend/src/main/java/com/ssafy/challs/domain/contest/repository/AwardsRepository.java
+++ backend/src/main/java/com/ssafy/challs/domain/contest/repository/AwardsRepository.java
@@ -1,8 +1,15 @@
 package com.ssafy.challs.domain.contest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.challs.domain.contest.entity.Awards;
+import com.ssafy.challs.domain.contest.entity.Contest;
 
 public interface AwardsRepository extends JpaRepository<Awards, Long> {
+
+	List<Awards> findAllByContest(Contest contest);
+
+	boolean deleteAllByContest(Contest contest);
 }
