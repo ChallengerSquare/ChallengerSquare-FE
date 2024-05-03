7485392ac2ce76d8e21342a9e097bd2c590f8909
CocoIsCat <demy176@naver.com>
2024-05-03 10:52:12 +0900

    [S10P31A205-219] Fix: 클래스 이름 Meta로 수정
    규약인 거 같은데 몰랐읍니다...

--- nodeList/apies/serializers.py
+++ nodeList/apies/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Node
 
 class NodeSerializer(serializers.ModelSerializer):
-    class Node:
+    class Meta:
         model = Node
         fields = ['ID', 'ADDR', 'LAST_CONN_DATE']
