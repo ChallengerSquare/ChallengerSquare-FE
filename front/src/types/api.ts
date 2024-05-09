748c5322083d8eaec44998fa1d5db2c351841349
김재윤 <jaeyun1723@naver.com>
2024-05-09 17:36:46 +0900

    fix : member.ts

--- front/src/types/api.ts
+++ front/src/types/api.ts
@@ -2,3 +2,10 @@ export interface ApiResponse {
   code: string
   data?: any
 }
+
+export interface MemberDto {
+  memberName: string
+  memberBirth: string
+  memberPhone: string
+  memberAddress: string
+}
