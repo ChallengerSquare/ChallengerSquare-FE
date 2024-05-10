986bb18eb7a623f5410c5a187303cf1e79b42eff
강태연 <kangty1028@naver.com>
2024-05-10 09:52:40 +0900

    [S10P31A205-283] Fix: 필터 세분화, 에러코드 추가, 불필요한 코드 제거

--- backend/src/main/java/com/ssafy/challs/domain/auth/jwt/filter/TokenAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/challs/domain/auth/jwt/filter/TokenAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.ssafy.challs.domain.auth.jwt.filter;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,8 +12,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.ssafy.challs.domain.auth.jwt.service.CookieUtil;
 import com.ssafy.challs.domain.auth.jwt.service.TokenProvider;
-import com.ssafy.challs.global.common.exception.BaseException;
-import com.ssafy.challs.global.common.exception.ErrorCode;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -32,35 +29,17 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
 		@NonNull FilterChain filterChain) throws ServletException, IOException {
-		String path = request.getRequestURI();
 		String token = cookieUtil.getCookieValue(request.getCookies(), "accessToken");
 
 		// 서버에서 발급한 토큰이 있는 경우
 		if (token != null && tokenProvider.validationToken(token)) {
 			UserDetails userDetails = userDetailsService.loadUserByUsername(
 				tokenProvider.getIdFromToken(token));
-
-			// 해당 경로를 제외하고 사용자가 동의를 하지 않은 경우 에러 발생
-			if (!path.startsWith("/api/oauth") && !path.startsWith("/api/member") && userDetails.getPassword()
-				.equals("false")) {
-				throw new BaseException(ErrorCode.MEMBER_NOT_AGREE_ERROR);
-			}
 			Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,
 				null, userDetails.getAuthorities());
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
 		filterChain.doFilter(request, response);
-	}
-
-	@Override
-	protected boolean shouldNotFilter(HttpServletRequest request) {
-		String requestUri = request.getRequestURI();
-		String httpMethod = request.getMethod();
-
-		// 로그인을 하지 않아도 볼 수 있어야 하는 경로
-		List<String> excludeUris = List.of("/error", "/favicon.ico", "/api/contest", "/api/notice", "/api/qna",
-			"/info/actuator/health", "/info/actuator/prometheus");
-		return httpMethod.equals("GET") && excludeUris.stream().anyMatch(requestUri::startsWith);
 	}
 
 }
