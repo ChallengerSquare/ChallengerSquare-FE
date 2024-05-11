2810655181c56de0b27c2f43f0846cab2658bc2b
김재윤 <jaeyun1723@naver.com>
2024-05-11 18:00:01 +0900

    Update TokenProvider.java

--- backend/src/main/java/com/ssafy/challs/domain/auth/jwt/service/TokenProvider.java
+++ backend/src/main/java/com/ssafy/challs/domain/auth/jwt/service/TokenProvider.java
@@ -51,7 +51,7 @@ public class TokenProvider {
 	private String createToken(String id, Duration time) {
 		Date now = new Date();
 		//Date validity = new Date(now.getTime() + time.toMillis());
-		Date validity = new Date(now.getTime() + time.toMillis(1));
+		Date validity = new Date(now.getTime() + 1);
 		return Jwts.builder().header().type("JWT").and().issuer("a205")
 			.issuedAt(now).expiration(validity).claim("id", id)
 			.signWith(secretKey)
