9a12106e992fa4bd60e38cccaf65c7cf2dd1c4db
남혜미 <nhm0525@naver.com>
2024-05-14 10:40:19 +0900

    [S10P31A205-392] feat : 대회 결과 검색 페이지 구현

---
+++ front/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styles from '@/components/SearchBar/SearachBar.module.scss'
+import searchIcon from '@/assets/search.svg'
+import { useRecoilState } from 'recoil'
+import { Search } from '@/types/search'
+import { searchState } from '@/pages/competition-search/store'
+
+interface SearchBarProps {
+  text: string
+  openBtn?: boolean
+  openBtnColor?: string
+  onClick: (searchQuery: string | undefined) => void
+  onChange?: (keyword: string) => void
+}
+
+const SearchBar = ({ text, openBtn, openBtnColor, onClick }: SearchBarProps) => {
+  const navigate = useNavigate()
+  const [search, setSearch] = useRecoilState<Search>(searchState)
+  const [keyword, setKeyword] = useState(search.keyword)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value)
+  }
+
+  const moveToComtetitionOpen = () => {
+    navigate(`/create-competition`)
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter' && keyword) {
+      // 'Enter' 키를 눌렀을 때만 검색 실행
+      onClick(keyword)
+    }
+  }
+
+  useEffect(() => {
+    setKeyword(search.keyword)
+  }, [search])
+
+  let checkColor = ''
+  if (openBtnColor === 'white') checkColor = styles.white
+  else checkColor = styles.mainColor
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.search_bar}>
+        <input
+          type="text"
+          placeholder={text}
+          value={keyword}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          maxLength={25}
+        />
+        <button type="button" onClick={() => onClick(keyword)}>
+          <img className={styles.searchimg} src={searchIcon} alt="검색" />
+        </button>
+      </div>
+      {openBtn == true ? (
+        <div className={styles.page_link}>
+          <button className={checkColor} type="button" onClick={() => moveToComtetitionOpen()}>
+            {`대회 개최하러가기 >`}
+          </button>
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export default SearchBar
--- front/src/pages/competition-result/CompetitionResult.tsx
+++ front/src/pages/competition-result/CompetitionResult.tsx
@@ -1,23 +1,32 @@
+import styles from '@/pages/competition-result/CompetitionResult.module.scss'
 import Navbar from '@/components/Navbar/Navbar'
+import blockchain from '@images/blockchain.jpg'
+import SearchBar from '@/components/SearchBar/SearchBar'
 
 const CompetitionResult = () => {
+  const handleSearch = () => {
+    console.log('test')
+  }
   return (
-    <>
-      <div className="bg-blue-500 min-h-screen">
+    <div className={styles.container}>
+      <div className={styles.navbar}>
         <Navbar />
-        <div>안녕</div>
-        <div>수상/참가 내역 조회 페이지</div>
       </div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-    </>
+      <div className={styles.body}>
+        <img className={styles.background} src={blockchain} alt={'background'} />
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <p>
+              <span>블록체인 코드 번호</span>를 통해
+            </p>
+            <p>참가/내역 조회 찾기</p>
+          </div>
+          <div className={styles.searchbar}>
+            <SearchBar text="원하는 대회를 입력해주세요." onClick={handleSearch} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
