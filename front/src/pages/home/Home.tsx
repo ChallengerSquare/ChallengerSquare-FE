edd653664e7faa52539e906c2282167daed1b3e4
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-05-15 23:00:12 +0900

    [S10P31A205-367] Feat: Basic Structure of main page

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -3,6 +3,8 @@ import { AxiosError } from 'axios'
 import { getUser } from '@/services/member'
 import Navbar from '@components/Navbar/Navbar'
 import { ApiResponse } from '@/types/api'
+import title from '../../../public/images/home/home-title.webp'
+import styles from './Home.module.scss'
 
 const Home = () => {
   // const { data, isLoading, isError, error } = useQuery<ApiResponse, AxiosError>('user', getUser)
@@ -11,20 +13,31 @@ const Home = () => {
   // if (isLoading) return <div>loading...</div>
   // if (isError) return <div>error:{error.message}</div>
   return (
-    <>
-      <div className="bg-blue-500 min-h-screen">
-        <Navbar enableScrollEffect />
-        <div>안녕</div>
-        <div>메인페이지</div>
+    <div className={styles.home}>
+      <div className={styles.main}>
+        <div className={styles.nav}>
+          <Navbar enableScrollEffect />
+        </div>
+        <div className={styles.title}>
+          <img src={title} alt="title" />
+        </div>
+        <div className={styles.mainDescription}>
+          <div>행사 개최, 관리, 참가를 한 곳에서.</div>
+          <div>수상, 참가 기록은 블록체인으로 보장해드려요.</div>
+        </div>
       </div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>4<div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-      <div>gg</div>
-    </>
+      <div className={styles.contents}>
+        <div className={styles.contentContainer}>
+          <div className={styles.photo}>
+            <div>이미지가 들어갈 거에요</div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.contentTitle}>행사 개최</div>
+            <div className={styles.contentDescription}>행사를 쉽게 개최하고 관리하세요.</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
