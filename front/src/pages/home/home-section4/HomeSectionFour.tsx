b718e84c299c5511a9006d27a20c1601a1b270b8
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-05-19 22:31:48 +0900

    [S10P31A205-477] Mod: thumbnails on information pages

--- front/src/pages/home/home-section4/HomeSectionFour.tsx
+++ front/src/pages/home/home-section4/HomeSectionFour.tsx
@@ -1,4 +1,4 @@
-import description from '@images/home/description.webp'
+import description from '@images/home/Description3.png'
 import styles from '@/pages/home/home-section4/HomeSectionFour.module.scss'
 
 const HomeSectionFour = () => {
@@ -9,7 +9,7 @@ const HomeSectionFour = () => {
       </div>
       <div className={styles.right}>
         <span>블록체인 기록 보장</span>
-        <p>수상, 참가 기록은 블록체인으로 보장해드려요!</p>
+        <p>수상, 참가 기록은 블록체인에 저장해요! 수상, 참가 내역을 스펙으로 사용하세요!</p>
       </div>
     </div>
   )
